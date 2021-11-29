2d9d33812dc7e87516a562e511b2af41a4e67de7
Juan Fran Martin <juanframaro@gmail.com>
2021-11-29 20:23:25 +0000

    added some pokemon routes

---
+++ src/router/index.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Pokemon from '../views/Pokemon.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/pokemon/:id',
+    name: 'Pokemon',
+    component: Pokemon
+  },
+
+]
+
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
---
+++ src/views/Pokemon.vue
@@ -0,0 +1,37 @@
+<template>
+<div>{{name}}</div>
+</template>
+<script>
+export default {
+  name: "Pokemon",
+  mounted() {
+    this.getPokemonData();
+  },
+  data() {
+    return {
+      id: 0,
+        name: '',
+    };
+  },
+  watch:{
+      $route(to){
+
+     this.id = Number(to.params.id);
+     this.getPokemonData();
+}},
+  methods: {
+    getPokemonData() {
+      console.log(this.id);
+      fetch(`https://pokeapi.co/api/v2/pokemon/${this.id}`)
+        .then(response => response.json())
+        .then(data => {
+            this.name = data.name;
+            console.log(data)
+        });
+
+    }
+  }
+}
+</script>
+<style>
+</style>
